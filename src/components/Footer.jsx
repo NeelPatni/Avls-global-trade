67113d9ee575eac98d3210d06dfdf293a0ca5e9b
Varun Yadav <yadav.varun056038@gmail.com>
2024-12-31 16:56:25 +0530

    fix minor issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -66,7 +66,7 @@ const Footer = () => {
                 </Link>
               </li>
               <li className="mb-2">
-                <Link href="/product-dettocardamom" className="hover:underline">
+                <Link href="/product-detail/cardamom" className="hover:underline">
                   Cardamom
                 </Link>
               </li>
